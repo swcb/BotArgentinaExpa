a9628f3790cf31aae6bd94760d009a3a28a7e22e
root <root@ip-172-31-30-148.us-west-2.compute.internal>
2020-03-20 22:47:53 +0000

    ok

--- startAll.sh
+++ startAll.sh
@@ -1,8 +1,7 @@
+
 #!/bin/bash
 
-# Script para inicializar todos os bots.
+# Script teste.
 
-pm2 start _get_approved_.py --interpreter python3 -f |
-pm2 start _get_applications_.py --interpreter python3 -f |
-pm2 start _get_accepted_.py --interpreter python3 -f |
-pm2 start _get_realized_.py --interpreter python3 -f
+echo Executando script teste
+pm2 list
