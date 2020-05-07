2b8aacd2beb8f5f9ea23ef5f44f8f0ad1542979d
Willian Caires <willinaika12@gmail.com>
2020-05-07 17:36:08 -0300

    ,

---
+++ Get Retroativo/2020/retro04.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script para pegar os approveds dados em 2019.
+
+echo "Obtendo approveds de 2019"
+
+#!/bin/bash
+
+# Script teste.
+
+echo Executando script teste
+
+pm2 start 2020_04_ACC.py --interpreter python3
+pm2 start 2020_04_APD.py --interpreter python3
+pm2 start 2020_04_APL.py --interpreter python3
+pm2 start 2020_04_RE.py --interpreter python3
